574174d517453f8ad6a73d1b78ab19164f083e80
Luke Clum <lukeclum@Lukes-MacBook-Pro.local>
2019-05-06 16:27:26 -0700

    storyform case study

--- src/app/projects-data.ts
+++ src/app/projects-data.ts
@@ -1,9 +1,9 @@
 import { Project } from './project';
 
 export const PROJECTS: Project[] = [
-  { name: 'Sous Vide Cook View', id: '/sous-vide-cook-view' },
-  { name: 'Storyform', id: '/storyform' },
-  { name: 'Joule Design System', id: '/joule-design-system' },
-  { name: 'Joule Packaging', id: '/joule-packaging' },
-  { name: 'Selected Work', id: '/selected-work' },
+  { name: 'Sous Vide Cook View', id: '/sous-vide-cook-view', thumb: '/assets/cook-flow.jpg' },
+  { name: 'Storyform', id: '/storyform', thumb: '/assets/storyform-devices.jpg' },
+  { name: 'Joule Design System', id: '/joule-design-system', thumb: '/assets/storyform-devices.jpg'  },
+  { name: 'Joule Packaging', id: '/joule-packaging', thumb: '/assets/storyform-devices.jpg'  },
+  { name: 'Selected Work', id: '/selected-work', thumb: '/assets/storyform-devices.jpg'  },
 ];

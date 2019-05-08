6bebd9bea3203da4be28ec877fb8ff06e8df69f8
Luke Clum <lukeclum@Lukes-MacBook-Pro.local>
2019-05-08 16:48:33 -0700

    rough draft of design system

--- src/app/projects-data.ts
+++ src/app/projects-data.ts
@@ -3,7 +3,6 @@ import { Project } from './project';
 export const PROJECTS: Project[] = [
   { name: 'Sous Vide Cook View', id: '/sous-vide-cook-view', thumb: '/assets/cook-flow.jpg' },
   { name: 'Storyform', id: '/storyform', thumb: '/assets/storyform-devices.jpg' },
-  { name: 'Joule Design System', id: '/joule-design-system', thumb: '/assets/storyform-devices.jpg'  },
+  { name: 'Joule Design System', id: '/joule-design-system', thumb: '/assets/ds-components.jpg'  },
   { name: 'Joule Packaging', id: '/joule-packaging', thumb: '/assets/joule-packaging-01.jpg'  },
-  { name: 'Selected Work', id: '/selected-work', thumb: '/assets/storyform-devices.jpg'  },
 ];

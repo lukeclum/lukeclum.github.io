3ad718be4f7fb18f6cf2ce59d7636ae7815da5ac
Luke Clum <lukeclum@Lukes-MacBook-Pro.local>
2019-05-16 11:26:37 -0700

    style changes

--- src/app/projects-data.ts
+++ src/app/projects-data.ts
@@ -5,24 +5,24 @@ export const PROJECTS: Project[] = [
     name: 'Joule Sous Vide',
     description: 'Redesigning the cooking experience for one of the world’s most popular sous vide devices.',
     id: '/sous-vide-cook-view',
-    thumb: '/assets/cook-flow.jpg'
+    thumb: '/assets/cv-thumb.jpg'
   },
   {
     name: 'Storyform',
     description: 'What I learned founding and failing a publishing startup.',
     id: '/storyform',
-    thumb: '/assets/storyform-devices.jpg'
+    thumb: '/assets/sf-thumb.jpg'
   },
   {
     name: 'Building A Design System',
     description: 'How I improved collaboration and efficiency through crafting a mobile design system from scratch.',
     id: '/joule-design-system',
-    thumb: '/assets/ds-thumbnail.jpg'
+    thumb: '/assets/ds-thumb.jpg'
   },
   {
     name: 'Joule Packaging',
     description: 'Redesigning packaging for retail',
     id: '/joule-packaging',
-    thumb: '/assets/joule-box-thumb.jpg'
+    thumb: '/assets/package-thumb.jpg'
   },
 ];
